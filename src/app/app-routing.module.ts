e2f6719720debdb97f93beaee45c67855e4eca47
AmbinintsoaZo <herizo@nomade.mg>
2023-10-02 16:04:42 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DocumentListComponent} from "./components/document-list/document-list.component";
+import {DocumentUploadComponent} from "./components/document-upload/document-upload.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'document', component: DocumentListComponent},
+  {path: 'document/upload', component: DocumentUploadComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocumentListComponent } from './components/document-list/document-list.component';
+import { DocumentUploadComponent } from './components/document-upload/document-upload.component';
+import { CategoryManagementComponent } from './components/category-management/category-management.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent,
+    DocumentUploadComponent,
+    CategoryManagementComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
