d8c13c2b44dbc697f56b76d628b37e2a4dfa9e0b
AmbinintsoaZo <herizo@nomade.mg>
2023-10-27 16:16:23 +0300

    updating sidebar lists

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import {Title} from "@angular/platform-browser";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,7 +8,86 @@ import {Title} from "@angular/platform-browser";
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  activePage: string = ''; // Initialize with an empty string
+  activePage: string = '';
+  navigationItems: any[] = [
+    {
+      route: '/dashboard',
+      label: 'Dashboard',
+      icon: 'dashboard',
+      i18nKey: 'dashboard',
+      title: 'PRM - Dashboard',
+    },
+    {
+      route: '/user/list',
+      label: 'User List',
+      icon: 'person_outline',
+      i18nKey: 'userList',
+      title: 'PRM - User Lists',
+    },
+    {
+      route: '/history',
+      label: 'History',
+      icon: 'receipt_long',
+      i18nKey: 'history',
+      title: 'PRM - Activities History',
+    },
+    {
+      route: '/analytics',
+      label: 'Analytics',
+      icon: 'insights',
+      i18nKey: 'analytics',
+      title: 'PRM - Analytics',
+    },
+    {
+      route: '/logement/list',
+      label: 'Logement',
+      icon: 'apartment',
+      i18nKey: 'logement',
+      title: 'PRM - Logement Lists',
+    },
+    {
+      route: '/apartment/list',
+      label: 'Apartment',
+      icon: 'apartment',
+      i18nKey: 'apartment',
+      title: 'PRM - Apartment Lists',
+    },
+    {
+      route: '/document/list',
+      label: 'Document',
+      icon: 'inventory',
+      i18nKey: 'document',
+      title: 'PRM - Documents Lists',
+    },
+    {
+      route: '/publish',
+      label: 'Publish',
+      icon: 'report_gmailerrorred',
+      i18nKey: 'publish',
+      title: 'PRM - Publish',
+    },
+    {
+      route: '/settings',
+      label: 'Settings',
+      icon: 'settings',
+      i18nKey: 'settings',
+      title: 'PRM - Settings',
+    },
+    {
+      route: '/new-login',
+      label: 'New Login',
+      icon: 'add',
+      i18nKey: 'newLogin',
+      title: 'PRM - New Login',
+    },
+    {
+      route: '/logout',
+      label: 'Logout',
+      icon: 'logout',
+      i18nKey: 'logout',
+      title: 'Logout',
+    }
+  ];
 
   constructor(private router: Router, private titleService: Title) { }
 
@@ -22,47 +101,46 @@ export class SidebarComponent implements OnInit {
     switch (route) {
       case '/dashboard':
         this.activePage = 'dashboard';
-        this.titleService.setTitle(`PRM - Dashboard`)
+        this.titleService.setTitle('PRM - Dashboard');
         break;
       case '/user/list':
         this.activePage = 'userList';
-        this.titleService.setTitle(`PRM - User Lists`)
+        this.titleService.setTitle('PRM - User Lists');
         break;
       case '/history':
         this.activePage = 'history';
-        this.titleService.setTitle(`PRM - Activities History`)
+        this.titleService.setTitle('PRM - Activities History');
         break;
       case '/analytics':
         this.activePage = 'analytics';
-        this.titleService.setTitle(`PRM - Analytics`)
+        this.titleService.setTitle('PRM - Analytics');
         break;
       case '/logement/list':
         this.activePage = 'logement';
-        this.titleService.setTitle(`PRM - Logement Lists`)
+        this.titleService.setTitle('PRM - Logement Lists');
         break;
       case '/apartment/list':
         this.activePage = 'apartment';
-        this.titleService.setTitle(`PRM - apartment Lists`)
+        this.titleService.setTitle('PRM - Apartment Lists');
         break;
       case '/document/list':
         this.activePage = 'document';
-        this.titleService.setTitle(`PRM - Documents Lists`)
+        this.titleService.setTitle('PRM - Documents Lists');
         break;
       case '/publish':
         this.activePage = 'publish';
-        this.titleService.setTitle(`PRM - Publish`)
+        this.titleService.setTitle('PRM - Publish');
         break;
       case '/settings':
         this.activePage = 'settings';
-        this.titleService.setTitle(`PRM - Settings`)
+        this.titleService.setTitle('PRM - Settings');
         break;
       case '/new-login':
         this.activePage = 'newLogin';
-        this.titleService.setTitle(`PRM - New Login`)
+        this.titleService.setTitle('PRM - New Login');
         break;
       default:
         this.activePage = ''; // Default to no active page
     }
   }
-
 }
